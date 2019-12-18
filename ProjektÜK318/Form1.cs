ae620964741ed2b8b9362271c85a102f38704b5d
nikolic <nikola111998@hotmail.com>
2019-12-18 16:28:32 +0100

    es wurde ein code geschrieben um die stationsvorschläge zu geben

--- ProjektÜK318/Form1.cs
+++ ProjektÜK318/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,62 @@ using System.Windows.Forms;
 
 namespace ProjektÜK318
 {
+    
+
     public partial class Form1 : Form
     {
+        private Transport transport = new Transport();
+
         public Form1()
         {
             InitializeComponent();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelDatum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonVerbindungsuchen_Click(object sender, EventArgs e)
+        {
+            var Abfahrtsort = Convert.ToString(comboBoxAbfahrtsort.Text);
+
+            var Route = Convert.ToString(transport.GetConnections(from VAR in Abfahrtsort ));
+            treeView1.Show(Route);
+        }
+
+        private void comboBoxAbfahrtsort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxAbfahrtsort_DropDown(object sender, EventArgs e)
+        {
+                var Abfahrtsort = Convert.ToString(comboBoxAbfahrtsort.Text);
+                transport.GetStations(Abfahrtsort);
+                Stations stations = transport.GetStations(Abfahrtsort);
+                comboBoxAbfahrtsort.Items.Clear();
+                foreach (Station station in stations.StationList)
+                {
+                    comboBoxAbfahrtsort.Items.Add(station.Name);
+                }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
         }
     }
 }
